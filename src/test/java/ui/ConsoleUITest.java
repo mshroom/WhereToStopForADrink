0515b62aacae64f3beb5f66016bdec91ae67432f
mshroom <32199029+mshroom@users.noreply.github.com>
2019-02-22 19:05:16 +0200

    added tests for ui

--- src/test/java/ui/ConsoleUITest.java
+++ src/test/java/ui/ConsoleUITest.java
@@ -145,7 +145,243 @@ public class ConsoleUITest {
         verify(algo, times(1)).compareShortestRouteAlgorithms(any(int[][].class), eq(false));
         verify(store, times(1)).createBigRandomCompleteGraph();
     }
+    
+    @Test
+    public void commandNewInSettingsMenuAsksForFileToImport() throws Exception {
+        inputs.add("settings");
+        inputs.add("new");
+        inputs.add("file.txt");
+        inputs.add("quit");
+        createUi();
+        verify(graphs, times(1)).importPlaces(eq("file.txt"));
+    }
+    
+    @Test
+    public void commandMemoryInSettingsMenuAccessesSavedFiles() throws Exception {
+        inputs.add("settings");
+        inputs.add("memory");
+        inputs.add("quit");
+        createUi();
+        verify(graphs, times(1)).useSaved(eq("data/userData/savedPlaces.txt"), eq("data/userData/savedGraph.txt"));
+    }
 
+    @Test
+    public void commandCustomInRouteMenuGivesErrorMessageIfCustomGraphIsNotSet() {
+        inputs.add("route");
+        inputs.add("custom");
+        inputs.add("quit");
+        createUi();
+        Queue<String> prints = io.getPrints();
+        assertTrue(prints.contains("You have not imported any graph."));
+    }    
+    
+    @Test
+    public void commandCustomInRouteMenuRequiresValidNumber() {
+        this.routeCustom();
+        inputs.add("30");
+        inputs.add("quit");
+        createUi();
+        Queue<String> prints = io.getPrints();
+        assertTrue(prints.contains("Not a valid number."));
+    }
+    
+    @Test
+    public void commandCustomInRouteMenuComparesOnlyApproximationAlgorithmsIfNumberIsTooBig() throws Throwable {
+        this.routeCustom();
+        inputs.add("16");
+        inputs.add("");
+        inputs.add("quit");
+        createUi();
+        verify(graphs, times(1)).getSmallerGraph(eq(16));
+        verify(algo, times(1)).compareShortestRouteAlgorithms(any(int[][].class), eq(false));
+    }
+    
+   
+    @Test
+    public void commandCustomInRouteMenuComparesAllAlgorithmsIfNumberIsSmall() throws Throwable {
+        this.routeCustom();
+        inputs.add("15");
+        inputs.add("");
+        inputs.add("quit");
+        createUi();
+        verify(graphs, times(1)).getSmallerGraph(eq(15));
+        verify(algo, times(1)).compareShortestRouteAlgorithms(any(int[][].class), eq(true));
+    }
+    
+    @Test
+    public void commandCustomInPathMenuGivesErrorMessageIfCustomGraphIsNotSet() {
+        inputs.add("path");
+        inputs.add("custom");
+        inputs.add("quit");
+        createUi();
+        Queue<String> prints = io.getPrints();
+        assertTrue(prints.contains("You have not imported any graph."));
+    }
+    
+    @Test
+    public void commandCustomInPathMenuRequiresValidNumber() {
+        this.pathCustom();
+        inputs.add("-1");
+        inputs.add("quit");
+        createUi();
+        Queue<String> prints = io.getPrints();
+        assertTrue(prints.contains("Not a valid number."));
+    }
+    
+    @Test
+    public void commandCustomInPathMenuComparesAlgorithmsWithCustomParameters() throws Throwable {
+        this.pathCustom();
+        inputs.add("15");
+        inputs.add("200");
+        inputs.add("a");
+        inputs.add("quit");
+        createUi();
+        verify(graphs, times(1)).getReducedGraph(eq(200));
+        verify(graphs, times(1)).getDistances(eq(15));
+        verify(algo, times(1)).compareShortestPathAlgorithms(any(int[][].class), eq(15), any(int[].class));
+    }
+    
+    @Test
+    public void commandSaveInSettingsMenuGivesErrorMessageIfCustomGraphIsNotSet() {
+        inputs.add("settings");
+        inputs.add("save");
+        inputs.add("quit");
+        createUi();
+        Queue<String> prints = io.getPrints();
+        assertTrue(prints.contains("You have not imported any graph."));
+    }
+    
+    @Test
+    public void commandSaveInSettingsMenuSavesCurrentGraph() throws Exception {
+        inputs.add("settings");
+        inputs.add("memory");
+        inputs.add("save");
+        inputs.add("quit");
+        createUi();
+        verify(graphs, times(1)).save(eq("data/userData/savedPlaces.txt"), eq("data/userData/savedGraph.txt"));   
+    }
+    
+    @Test
+    public void commandHomeInSettingsMenuGivesErrorMessageIfCustomGraphIsNotSet() {
+        inputs.add("settings");
+        inputs.add("home");
+        inputs.add("quit");
+        createUi();
+        Queue<String> prints = io.getPrints();
+        assertTrue(prints.contains("You have not imported any graph."));
+    }    
+    
+    @Test
+    public void commandHomeInSettingsMenuAsksForNewAddressAndChangesIt() throws Throwable {
+        inputs.add("settings");
+        inputs.add("memory");
+        inputs.add("home");
+        inputs.add("yes");
+        inputs.add("My New Address");
+        inputs.add("quit");
+        createUi();
+        verify(graphs, times(1)).changeHomeAddress(eq("My New Address"));
+    }
+    
+    @Test
+    public void commandHomeInSettingsMenuDoesNotChangeAddressIfGivenEmptyInput() throws Throwable {
+        inputs.add("settings");
+        inputs.add("memory");
+        inputs.add("home");
+        inputs.add("yes");
+        inputs.add("");
+        inputs.add("quit");
+        createUi();
+        verify(graphs, times(0)).changeHomeAddress(eq(""));
+    }
+    
+    @Test
+    public void commandAddInSettingsMenuGivesErrorMessageIfCustomGraphIsNotSet() {
+        inputs.add("settings");
+        inputs.add("add");
+        inputs.add("quit");
+        createUi();
+        Queue<String> prints = io.getPrints();
+        assertTrue(prints.contains("You have not imported any graph."));
+    }
+    
+    @Test
+    public void commandAddInSettingsMenuAsksForNewPlaceNameAndAddressAndAddsTheNewPlace() {
+        inputs.add("settings");
+        inputs.add("memory");
+        inputs.add("add");
+        inputs.add("Name of the place");
+        inputs.add("Address");
+        inputs.add("quit");
+        createUi();
+        verify(graphs, times(1)).addPlace(eq("Name of the place"), eq("Address"));
+    }
+    
+    @Test
+    public void commandAddInSettingsMenuDoesNotAddAPlaceWithEmptyName() {
+        inputs.add("settings");
+        inputs.add("memory");
+        inputs.add("add");
+        inputs.add("");
+        inputs.add("Address");
+        inputs.add("quit");
+        createUi();
+        verify(graphs, times(0)).addPlace(eq(""), eq("Address"));
+    }
+    
+    @Test
+    public void commandAddInSettingsMenuDoesNotAddPlaceWithEmptyAddress() {
+        inputs.add("settings");
+        inputs.add("memory");
+        inputs.add("add");
+        inputs.add("Name of the place");
+        inputs.add("");
+        inputs.add("quit");
+        createUi();
+        verify(graphs, times(0)).addPlace(eq("Name of the place"), eq(""));    
+    }
+    
+    @Test
+    public void commandFindInPathMenuGivesErrorMessageIfCustomGraphIsNotSet() {
+        inputs.add("path");
+        inputs.add("find");
+        inputs.add("quit");
+        createUi();
+        Queue<String> prints = io.getPrints();
+        assertTrue(prints.contains("You have not imported any graph."));
+    }
+    
+    @Test
+    public void commandFindInPathMenuMakesASearchWithGivenWordAndPrintsFoundPlaces() {
+        inputs.add("settings");
+        inputs.add("memory");
+        inputs.add("back");
+        inputs.add("path");
+        inputs.add("find");
+        inputs.add("place");
+        inputs.add("1");
+        inputs.add("200");
+        inputs.add("");
+        inputs.add("quit");
+        createUi();
+        verify(graphs, times(1)).findPlaces("place");
+        Queue<String> prints = io.getPrints();
+        assertTrue(prints.contains("[1] A Place"));
+    }
+    
+    @Test
+    public void commandFindInPathMenuCancelsSearchWithEmptyInput() {
+        inputs.add("settings");
+        inputs.add("memory");
+        inputs.add("back");
+        inputs.add("path");
+        inputs.add("find");
+        inputs.add("");
+        inputs.add("quit");
+        createUi();
+        verify(graphs, times(0)).findPlaces("");
+    }
+    
     private void createUi() {
         io = new StubIO(inputs);
         algo = mock(AlgorithmController.class);
@@ -163,5 +399,26 @@ public class ConsoleUITest {
         when(store.createSmallCompleteGraph()).thenReturn(new int[1][1]);
         when(store.createBigCompleteGraph()).thenReturn(new int[1][1]);
         when(store.createBigRandomCompleteGraph()).thenReturn(new int[1][1]);
+        when(graphs.getSizeOfCurrentGraph()).thenReturn(20);
+        when(graphs.getSmallerGraph(anyInt())).thenReturn(new int[1][1]);   
+        when(graphs.getReducedGraph(anyInt())).thenReturn(new int[1][1]);
+        when(graphs.getDistances(anyInt())).thenReturn(new int[1]);
+        when(graphs.findPlaces(eq("place"))).thenReturn("[1] A Place");
+    }
+    
+    private void routeCustom() {
+        inputs.add("settings");
+        inputs.add("memory");
+        inputs.add("back");
+        inputs.add("route");
+        inputs.add("custom");
+    }
+    
+    private void pathCustom() {
+        inputs.add("settings");
+        inputs.add("memory");
+        inputs.add("back");
+        inputs.add("path");
+        inputs.add("custom");
     }
 }
